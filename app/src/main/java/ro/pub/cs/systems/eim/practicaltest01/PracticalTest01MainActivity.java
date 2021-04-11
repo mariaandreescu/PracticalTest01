1e9ebaff3c6e7a797a9fb75be8f2b526f3704a1a
mariaandreescu <maria.andreescu@stud.acs.upb.com>
2021-04-11 20:28:55 +0300

    'B1. + B2.'

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/PracticalTest01MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest01/PracticalTest01MainActivity.java
@@ -2,13 +2,76 @@ package ro.pub.cs.systems.eim.practicaltest01;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class PracticalTest01MainActivity extends AppCompatActivity {
+
+    private EditText leftEditText;
+    private EditText rightEditText;
+    private Button pressMeButton, pressMeTooButton;
+    private Button navigateToSecondaryActivityButton;
+
+    PressButtonListener buttonListener = new PressButtonListener();
+
+    private class PressButtonListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            int leftNumberOfClicks = Integer.valueOf(leftEditText.getText().toString());
+            int rightNumberOfClicks = Integer.valueOf(rightEditText.getText().toString());
+
+            switch (view.getId()) {
+                case R.id.press_me_button:
+                    leftNumberOfClicks++;
+                    leftEditText.setText(String.valueOf(leftNumberOfClicks));
+                    break;
+                case R.id.press_me_too_button:
+                    rightNumberOfClicks++;
+                    rightEditText.setText(String.valueOf(rightNumberOfClicks));
+                    break;
+//                case R.id.navigate_to_secondary_activity_button:
+//                    Intent intent = new Intent(getApplicationContext(), PracticalTest01SecondaryActivity.class);
+//                    int numberOfClicks = Integer.parseInt(leftEditText.getText().toString()) +
+//                            Integer.parseInt(rightEditText.getText().toString());
+//                    intent.putExtra(Constants.NUMBER_OF_CLICKS, numberOfClicks);
+//                    startActivityForResult(intent, Constants.SECONDARY_ACTIVITY_REQUEST_CODE);
+//                    break;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_main);
+
+        leftEditText = (EditText) findViewById(R.id.left_edit_text);
+        rightEditText = (EditText) findViewById(R.id.right_edit_text);
+        pressMeButton = (Button) findViewById(R.id.press_me_button);
+        pressMeButton.setOnClickListener(buttonListener);
+        pressMeTooButton = (Button) findViewById(R.id.press_me_too_button);
+        pressMeTooButton.setOnClickListener(buttonListener);
+
+        //salvarea starii
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(Constants.LEFT_COUNT)) {
+                leftEditText.setText(savedInstanceState.getString(Constants.LEFT_COUNT));
+            } else {
+                leftEditText.setText(String.valueOf(0));
+            }
+            if (savedInstanceState.containsKey(Constants.RIGHT_COUNT)) {
+                rightEditText.setText(savedInstanceState.getString(Constants.RIGHT_COUNT));
+            } else {
+                rightEditText.setText(String.valueOf(0));
+            }
+        } else {
+            leftEditText.setText(String.valueOf(0));
+            rightEditText.setText(String.valueOf(0));
+        }
     }
 }
+
